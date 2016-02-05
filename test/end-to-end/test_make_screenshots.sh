6ef881b2005aca02dca68f81d9db5cbd81d627a7
Ino de Bruijn <ino@ino.pm>
2016-02-05 16:33:50 -0500

    Fix make_screenshots path in test

--- test/end-to-end/test_make_screenshots.sh
+++ test/end-to-end/test_make_screenshots.sh
@@ -6,14 +6,14 @@ set -e
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 # patient view screenshot
-phantomjs --ignore-ssl-errors=true make_screenshot.js 'http://localhost:8080/case.do?cancer_study_id=lgg_ucsf_2014&case_id=P04' \
+phantomjs --ignore-ssl-errors=true ${DIR}/make_screenshot.js 'http://localhost:8080/case.do?cancer_study_id=lgg_ucsf_2014&case_id=P04' \
                               "${DIR}/screenshots/patient_view_lgg_ucsf_2014_case_id_P04.png" \
                               5000
 # make sure screenshot is still the same as the one in the repo
 git diff --quiet -- ${DIR}/screenshots/patient_view_lgg_ucsf_2014_case_id_P04.png
 
 # studie view screenshot
-phantomjs --ignore-ssl-errors=true --web-security=false make_screenshot.js 'http://localhost:8080/study.do?cancer_study_id=lgg_ucsf_2014' \
+phantomjs --ignore-ssl-errors=true --web-security=false ${DIR}/make_screenshot.js 'http://localhost:8080/study.do?cancer_study_id=lgg_ucsf_2014' \
                               "${DIR}/screenshots/study_view_lgg_ucsf_2014.png" \
                               5000
 git diff --quiet -- ${DIR}/screenshots/study_view_lgg_ucsf_2014.png
